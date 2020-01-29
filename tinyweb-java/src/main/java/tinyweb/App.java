5fe556b34e757c89218ebd08b6df560bcc94c933
Marc Schanne <marc@schanne.org>
2020-01-29 22:35:39 +0100

    Format and prepare Perl-Adapter

--- tinyweb-java/src/main/java/tinyweb/App.java
+++ tinyweb-java/src/main/java/tinyweb/App.java
@@ -4,10 +4,10 @@ package tinyweb;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
 }
