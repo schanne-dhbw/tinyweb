1353a4603943a9830ba19af7c4feb18ce399cf66
Marc Schanne <marc@schanne.org>
2020-10-31 16:34:46 +0100

    Build process for scala project changed

--- tinyweb-scala/build.bat
+++ tinyweb-scala/build.bat
@@ -1,5 +1,1 @@
-start cmd /c "cd D:\DHBW\TinyWeb\tinyweb-scala\src\main\scala & scalac -d ..\..\..\target\classes\ com\mblinn\mbfpp\oo\tinyweb\*.scala"
-
-start cmd /c "cd D:\DHBW\TinyWeb\tinyweb-scala\src\main\scala & scalac -cp ..\..\..\target\classes\ -d ..\..\..\target\classes\ com\mblinn\mbfpp\oo\tinyweb\example\Example.scala"
-
-start cmd /c "cd D:\DHBW\TinyWeb\tinyweb-scala\src\main\scala & scalac -cp .;..\..\..\target\classes\ -d ..\..\..\target\classes\ tinyweb\*.scala"
+cd D:\DHBW\TinyWeb\tinyweb-scala\src\main\scala && scalac -d ..\..\..\target\classes\ com\mblinn\mbfpp\oo\tinyweb\*.scala && scalac -cp ..\..\..\target\classes\ -d ..\..\..\target\classes\ com\mblinn\mbfpp\oo\tinyweb\example\Example.scala && scalac -cp .;..\..\..\target\classes\ -d ..\..\..\target\classes\ tinyweb\*.scala && cd D:\DHBW\TinyWeb\tinyweb-scala\
